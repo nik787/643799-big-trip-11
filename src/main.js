69e70e576f5ff5b4c8a3bb38fbadf81cbce7c628
nik787 <nikito101d@gmail.com>
2020-04-02 11:03:46 +0300

    Добавляет меню и фильтры

--- src/main.js
+++ src/main.js
@@ -28,6 +28,38 @@ const tripInfoCostTemplate = () => {
   );
 };
 
+const tripMainNavTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn" href="#">Table</a>
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
+    </nav>`
+  );
+};
+
+const tripMainFilterTemplate = () => {
+  return (
+    `<form class="trip-filters" action="#" method="get">
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`
+  );
+};
+
 const pageBody = document.querySelector('.page-body');
 const pageHeader = pageBody.querySelector('.page-header');
 const pageHeaderTripMain = pageHeader.querySelector('.trip-main');
@@ -39,4 +71,9 @@ const TripInfoContainer = pageHeaderTripMain.querySelector('.trip-info');
 render(TripInfoContainer, tripInfoMainTemplate());
 render(TripInfoContainer, tripInfoCostTemplate());
 
+const tripMainControl = pageHeaderTripMain.querySelector('.trip-main__trip-controls');
+const tripMainNavTitle = tripMainControl.querySelector('h2');
+
+render(tripMainNavTitle, tripMainNavTemplate(), `afterend`);
+render(tripMainControl, tripMainFilterTemplate());
 
